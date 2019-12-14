358ce19df725159aca7636ac25b66197329039ce
emanovic7 <e_owusu@live.com>
2019-12-14 16:26:01 -0500

    add long form solution

---
+++ leetcode/jewelsAndStones.js
@@ -0,0 +1,34 @@
+
+//QUESTION
+
+// You're given strings J representing the types of stones that are jewels,
+// and S representing the stones you have.  Each character in S is a type of stone you have.
+// You want to know how many of the stones you have are also jewels.
+// The letters in J are guaranteed distinct, and all characters in J and S are letters.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+//
+// Example 1:
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+//
+// Example 2:
+// Input: J = "z", S = "ZZ"
+// Output: 0
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+
+ const numOfJewelsInStones = (J, S) => {
+   //long way:
+   let count = 0;
+   for(let i=0; i<J.length; i++){
+     for(let j=0; j<S.length; j++){
+       if(J[i] === S[j]){
+         count++
+       }
+     }
+   }
+   return count
+ }
