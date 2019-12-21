5329fc9161a85a9c3cf09f93c5dce3a75ccc4c63
emanovic7 <e_owusu@live.com>
2019-12-21 15:19:26 -0500

    add hashmaps implementation

---
+++ hashMaps/hashMaps.js
@@ -0,0 +1,45 @@
+class HashTable {
+  constructor(size){
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i =0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    //pass the key through the hash function to create an address
+    //wher we will store our value in the table.
+    let address = this._hash(key);
+    //check to see if the address is empty
+    if (!this.data[address]) {
+      //if it is, set it to an empty array
+      this.data[address] = [];
+    }
+    //put the key-value pair into the empty address
+    //created by the hash function
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key){
+    //pass the key through the hash function to create an address
+    const address = this._hash(key);
+    //set a variable currentBucket to the address returned by the hash function
+    const currentBucket = this.data[address]
+     //if currentBucket isn't empty
+    if (currentBucket) {
+      for(let i = 0; i < currentBucket.length; i++){
+        if(currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    }
+    //if there's nothing in the bucket(array), return undefined
+    return undefined;
+  }
+}
