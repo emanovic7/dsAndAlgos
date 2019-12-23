6ed1985289e4b689c9c91c050fdebde698c37b30
emanovic7 <e_owusu@live.com>
2019-12-23 17:57:34 -0500

    add long solution to evenNumberOfDigits problem

--- leetcode/simpleAnagram.js
+++ leetcode/simpleAnagram.js
@@ -8,3 +8,27 @@
 const checkIfAnagram = (A, B) => {
   return A.split('').sort().join('') === B.split('').sort().join('')
 }
+
+
+//SOLUTION 2, using hash maps
+const checkIfAnagram = (A, B) => {
+      let finalWord = []
+      let wordsHash = {}
+      let word1 = A.split('');
+      let word2 = B.split('')
+
+      for(let i=0; i<word1.length; i++){
+          let currLetter = word1[i]
+          if(wordsHash[currLetter] === undefined){
+              wordsHash[currLetter] = currLetter;
+          }
+      }
+
+      for(let j=0; j<word2.length; j++){
+        let currLetter = word2[j];
+          if(wordsHash[currLetter]){
+             finalWord.push(currLetter)
+          }
+      }
+      return finalWord.join('').length === word1.length;
+  }
