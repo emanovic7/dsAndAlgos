ac410311bc18addf0f0500e6b5ce2c5bf428a78e
emanovic7 <e_owusu@live.com>
2019-12-26 11:40:27 -0500

    add contains duplicate solution

---
+++ leetcode/containsDuplicate.js
@@ -0,0 +1,30 @@
+//QUESTION
+
+// Given an array of integers, find if the array contains any duplicates.
+// Your function should return true if any value appears at least twice in the array,
+// and it should return false if every element is distinct.
+
+// Example 1:
+// Input: [1,2,3,1]
+// Output: true
+//
+// Example 2:
+// Input: [1,2,3,4]
+// Output: false
+//
+// Example 3:
+// Input: [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+const containsDuplicate = (nums) => {
+  let hash = {};
+
+    for(let i=0; i<nums.length; i++){
+        if(hash[nums[i]] !== undefined){
+            return true;
+        }else {
+            hash[nums[i]] = nums[i];
+        }
+    }
+    return false;
+}
